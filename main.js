5ce8b41d01d1f8dbe02b382c506d32ede418faf1
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-07-27 07:57:40 -0600

    Add Clear All functionality and enable/disable Make Task List

--- main.js
+++ main.js
@@ -16,36 +16,78 @@ container.addEventListener('click', containerClickHandler);
 
 //Functions
 function headerKeyupHandler(e) {
-
+  if (e.target.id === 'search-input'){
+    searchArticles();
+  };
 };
 
 function headerClickHandler(e) {
-  e.preventDefault()
+  e.preventDefault();
 };
 
 function navClickHandler(e) {
-  e.preventDefault()
-  if (e.target.id === 'add-task-btn'){
-    addTaskToNav()
-    clearTaskItem()
-  }
+  e.preventDefault();
+  
+  if (e.target.id === 'add-task-btn') {
+    addTaskToNav();
+    toggleMakeTaskBtn();
+  };
+  
+  if (e.target.id === 'make-task-btn') {
+    makeTaskList();
+    removeNavLi();
+    clearTaskItem();
+    clearTaskTitle();
+  };
+
+  if (e.target.id === 'clear-btn'){
+    clearNav();
+  };
+};
+
+
+function makeTaskList() {
+  
 };
 
 function navKeyupHandler(e) {
-
+ if (e.target.id === 'task-title-input'  ||  e.target.id === 'task-item-input')
+  toggleMakeTaskBtn()
 };
 
 function containerClickHandler(e) {
   e.preventDefault()
 };
 
-function addTaskToNav(){
-  // e.preventDefault()
-  var taskId = Date.now();
-  makeTask(taskId);
+
+function toggleMakeTaskBtn() {
+  if (document.querySelector('#task-title-input').value !== '' && listContainer.innerHTML !== ''){
+    document.querySelector('#make-task-btn').classList.remove('disabled');
+    document.querySelector('#make-task-btn').disabled = false;
+  } else {
+    document.querySelector('#make-task-btn').classList.add('disabled');
+    document.querySelector('#make-task-btn').disabled = true;
+  }; 
 };
 
-function makeTask(taskId) {
+
+function removeNavLi() {
+  document.querySelector('.list__container').innerHTML = '';
+};
+
+function clearTaskTitle() {
+  document.querySelector('#task-title-input').value = '';
+}
+
+function addTaskToNav() {
+  if (document.querySelector('#task-item-input').value !== ''){
+    var taskId = Date.now();
+    makeNavTask(taskId);
+    clearTaskItem();
+  };  
+};
+
+function makeNavTask(taskId) {
   listContainer.insertAdjacentHTML('beforeend',
     `<li class="nav__li" data-id="${taskId}" id="${taskId}">
         <input type='image' src='images/delete.svg' class='nav__li--delete'>
@@ -57,8 +99,9 @@ function clearTaskItem() {
   document.querySelector('#task-item-input').value = '';
 };
 
-
-function makeTaskList(){
-
+function clearNav() {
+  removeNavLi();
+  clearTaskTitle();
+  clearTaskItem();
+  toggleMakeTaskBtn()
 };
-

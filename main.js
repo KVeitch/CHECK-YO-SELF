f28defd2c9d26407efadeea3af1f774abbdc9a5d
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-07-27 15:19:06 -0600

    Add taskArray reinitilization filled with todo class objects and repopulate the container with todoLists

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Global Variables
-var taskArray = JSON.parse(localStorage.getItem('tasks')) || [];
+var taskArray = [];
 
 // Document Query Selector
 var header = document.querySelector('#header')
@@ -8,16 +8,21 @@ var container = document.querySelector('.card__holder');
 var listContainer = document.querySelector('.list__container');
 
 //Event Listeners
+window.addEventListener('load', repopulate);
 header.addEventListener('keyup', headerKeyupHandler);
 nav.addEventListener('click', navClickHandler);
 nav.addEventListener('keyup', navKeyupHandler);
 container.addEventListener('click', containerClickHandler);
 
-//Functions
+//Event Handler Functions
 function headerKeyupHandler(e) {
   if (e.target.id === 'search-input'){
     searchArticles();
   };
+};
+
+function containerClickHandler(e) {
+  e.preventDefault()
 };
 
 function navClickHandler(e) {
@@ -40,19 +45,86 @@ function navClickHandler(e) {
   };
 };
 
-
-function makeTaskList() {
-
-};
-
 function navKeyupHandler(e) {
  if (e.target.id === 'task-title-input'  ||  e.target.id === 'task-item-input')
-  toggleMakeTaskBtn()
+  toggleMakeTaskBtn();
 };
 
-function containerClickHandler(e) {
-  e.preventDefault()
+// Other Functions
+function repopulate() {
+  initialTaskArray();
+  persistOnLoad();
 };
+
+function persistOnLoad() {
+  taskArray.forEach(function(taskList) {
+   displayToDo(taskList);
+  });
+};
+
+function displayToDo(taskList) {
+  var urgent;
+  var ugrentClass;
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<article class="container__article" data-id="${taskList.id}" id="${taskList.id}">
+      <header class="article__header--title">
+        <h3 class="">${taskList.title}</h3>
+      </header>
+      <!-- List inserts under here -->
+      <ul class="crd--ul" id="" data-id=""> 
+        <li>Card pop</li>                        
+      </ul>
+      <footer class="article__footer bottom--crd--stn">
+        <div class="article__div--urgent">
+          <input type="image" src="images/urgent.svg" id="btn--urgent" class="article__input--btn article__urgent">
+          <p class="article__footer--text">URGENT</p>
+        </div>
+        <div class="article__div--delete">
+          <input type="image" src="images/delete.svg" id="btn--delete" class="article__input--btn article__delete" disabled="">
+          <p class="article__footer--text">DELETE</p>
+        </div>
+      </footer>
+    </article>`
+  );
+};
+
+
+function initialTaskArray() {
+  if (JSON.parse(localStorage.getItem('tasks')) === null) {
+    taskArray = [];
+  } else {
+    taskArray = JSON.parse(localStorage.getItem('tasks')).map(element => new ToDo(element));
+  };
+};
+
+
+
+function makeTaskList() {
+  var tasksList = getTasksFromNav();
+  var todoList = new ToDo({id:Date.now(),
+                           title: document.querySelector('#task-title-input').value,
+                           tasksList: tasksList,
+                           urgernt: false,
+                          });
+  taskArray.push(todoList)
+  todoList.saveToStorage(taskArray);
+
+};
+
+function getTasksFromNav(){
+  var taskList = [];
+  var tasksOnNav = document.querySelectorAll('.nav__li');
+  tasksOnNav.forEach(function(li){
+    taskList.push({taskId: li.id,
+                   taskText: li.innerText,
+                   checked: false,
+                  });
+    });
+  return taskList; 
+
+};
+
 
 
 function toggleMakeTaskBtn() {
@@ -65,14 +137,13 @@ function toggleMakeTaskBtn() {
   }; 
 };
 
-
 function removeNavLi() {
   document.querySelector('.list__container').innerHTML = '';
 };
 
 function clearTaskTitle() {
   document.querySelector('#task-title-input').value = '';
-}
+};
 
 function addTaskToNav() {
   if (document.querySelector('#task-item-input').value !== ''){
